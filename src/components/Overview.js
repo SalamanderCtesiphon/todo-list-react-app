371708f032f66e01134efcb9f07a4829229c7535
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-17 10:26:33 -0600

    delted unused files and set up my own

---
+++ src/components/Overview.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Overview extends Component {
+  render() {
+    return (
+      <div>Overview</div>
+    )
+  }
+}
+
+export default Overview
