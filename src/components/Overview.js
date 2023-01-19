0f844e75aa177927a3ba5a1edc0de1f7bbb16add
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-19 11:34:11 -0600

    formated the script

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -9,13 +9,13 @@ class Overview extends Component {
       <ul>
         {tasks.map((task) => {
           return <li key={task.id}>
-                  <div>{(tasks.indexOf(task) + 1 )}.{' '}{task.text}</div>
-                  <div className='btnBox'>
-                    <button onClick={this.props.delTask.bind(this, task.id)}
-                    >Delete</button>
-                    <button onClick={this.props.editTask.bind(this, task.id)}>Edit</button>
-                  </div>
-                 </li>;
+            <div>{(tasks.indexOf(task) + 1 )}.{' '}{task.text}</div>
+            <div className='btnBox'>
+              <button onClick={this.props.delTask.bind(this, task.id)}
+              >Delete</button>
+              <button onClick={this.props.editTask.bind(this, task.id)}>Edit</button>
+            </div>
+            </li>;
         })}
       </ul>
     );
