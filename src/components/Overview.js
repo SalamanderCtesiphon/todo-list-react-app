5a1c4cc2d000cf1361d48914d4cd5754ce808d59
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-20 11:22:51 -0600

    add conditional rendering to the list that shows a all tasks complete message when all tasks are deleted

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,19 +6,29 @@ class Overview extends Component {
   render() {
     const { tasks, task } = this.props;
 
-    return (
-      <ul>
-        {tasks.map((task) => {
-            return <TaskItem tasks={tasks} task={task} 
-            delTask={this.props.delTask}
-            editTask={this.props.editTask}/>
-          
-        })}
-      </ul>
-    );
+    if (tasks.length == 0) {
+      return (
+        <ul>
+          <li>No more tasks for now ...</li>
+        </ul>
+      )
+    } else {
+      return (
+        <ul>
+          {tasks.map((task) => {
+              return <TaskItem tasks={tasks} task={task} 
+              delTask={this.props.delTask}
+              editTask={this.props.editTask}/>
+            
+          })}
+        </ul>
+      );
+      }
+    
   }
 
   
 };
 
 export default Overview;
+
