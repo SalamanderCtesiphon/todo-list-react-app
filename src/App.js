b511bb703321c0093b760472e21264cf0c29b91d
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-22 15:06:58 -0600

    reverted back to the simple app from the assignment I got too lost in the weeds to continue down the path I was on so I am going to try to do this again and hopefully not get lost in minutia>?????

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import './App.css'
-import AddNewTask from './components/AddNewTask'
-import uniqid from 'uniqid'
+import React, { Component } from "react";
+import Overview from "./components/Overview";
+import Header from "./components/Header";
+import uniqid from "uniqid";
+import "./App.css";
 
-export class App extends Component {
-
-  constructor(props) {
-    super(props)
+class App extends Component {
+  constructor() {
+    super();
 
     this.state = {
-      tasks: [
-      ],
-    }
-  }
-  
-  
-
-  delTask = (id) => {
-    this.setState({ 
-      tasks: [...this.state.tasks.filter(task => task.id !==id)]})
-  } 
-
-  addItem = (text) => {
-    const newTask = {
-      id: uniqid(),
-      text,
-      edit: false
-    }
-    this.setState({ tasks: [...this.state.tasks, newTask]})
+      task: {
+        text: '', 
+        id: uniqid()
+      },
+      tasks: [],
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '', 
+        id: uniqid()
+      },
+    });
+  };
 
   render() {
-    const { tasks } = this.state;
+    const { task, tasks } = this.state;
 
     return (
-      <div className='App'>
+      <div className="App">
         <Header />
-        <div className='inputForm'>
-          <AddNewTask 
-          tasks={tasks}
-          delTask={this.delTask}
-          addItem={this.addItem}
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
           />
-        </div>
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview tasks={tasks} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,39 +1,15 @@
-import React, { Component } from 'react'
-import TaskItem from './TaskItem';
+import React from "react";
 
-class Overview extends Component {
-  
-  render() {
-    const { tasks } = this.props;
+const Overview = (props) => {
+  const { tasks } = props;
 
-    if (tasks.length === 0) {
-      return (
-        <ul>
-          <li key={this.props.task.id}>All tasks are complete.
-          </li>
-        </ul>
-      )
-    } else {
-      return (
-        <ul>
-          {tasks.map((task) => {
-              return <TaskItem tasks={tasks} task={task}
-              key={this.props.task.id} 
-              id={this.props.task.id}
-              edit={this.props.task.edit}
-              delTask={this.props.delTask}
-              editTask={this.props.editTask}
-              rehandleChange={this.props.rehandleChange}
-              />
-          })}
-        </ul>
-      );
-      }
-    
-  }
-
-  
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default Overview;
-
