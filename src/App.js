e075451be43d00508310f35ce7d8a036543fc00d
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-27 15:03:15 -0600

    burned it all down and I am starting over using functional components and hooks

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
-import React, { Component } from "react";
-import Overview from "./components/Overview";
-import Header from "./components/Header";
-import uniqid from "uniqid";
-import "./App.css";
+import './App.css'
+import Overview from './components/Overview'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      task: {
-        text: '', 
-        id: uniqid()
-      },
-      tasks: [],
-    };
-
-    this.delTask = this.delTask.bind(this);
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      },
-    });
-  };
-
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: {
-        text: '', 
-        id: uniqid()
-      },
-    });
-  };
-
-  delTask = (id) => {
-    this.setState({ tasks: [...this.state.tasks.filter(task => task.id !==id)]})
-  }
-
-  render() {
-    const { task, tasks } = this.state;
-
-    return (
-      <div className="App">
-        <Header />
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task:</label>
-          <input
-            onChange={this.handleChange}
-            value={task.text}
-            type="text"
-            id="taskInput"
-          />
-          <button type="submit">Add Task</button>
-        </form>
-        <Overview tasks={tasks} 
-        delTask={this.delTask}
-        />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Task App</h1>  
+      <Overview />    
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,39 +1,7 @@
-import React, { Component } from "react";
+const Overview = () => {
+  return (
+    <p>this is the Overview</p>
+  )
+}
 
-class Overview extends Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { tasks, id } = this.props;
-
-  if (tasks.length === 0) {
-    return (
-      <ul>
-        <li>You have completed all tasks.</li>
-      </ul>
-      
-    )
-  } else {
-    return (
-      <ul>
-        {tasks.map((task) => {
-          return <li key={task.id}>
-            {tasks.indexOf(task) + 1}. {''} {task.text}
-            <button onClick={this.props.delTask.bind(this, task.id)}>Remove</button>        
-          </li>;
-        })}
-      </ul>
-    );
-  }
-
-  
-
-  }
-
-  
-};
-
-export default Overview;
+export default Overview
